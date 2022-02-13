efd92f1fe7c4ddd2044cb378851e7600ac579ba7
Nihatcan DALYANLAR <ndalyanlar@gmail.com>
2022-02-13 12:01:06 +0300

    commm

--- lib/Core/patient_model_view.dart
+++ lib/Core/patient_model_view.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:hemsirem/Model/appointment.dart';
 
+class NurseModelView extends ChangeNotifier {
+  late Appointment _appointment;
+
+  void addProgress(Appointment appointment) {
+    _appointment = appointment;
+    notifyListeners();
+  }
+}
